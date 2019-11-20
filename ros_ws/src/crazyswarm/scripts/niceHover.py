2f4cc312d8bb2c80432675851ac0d09f68efda10
Enrica <enrica.s92@gmail.com>
2019-11-20 10:46:31 +0100

    Commented out wait until button pressed in niceHover.py

--- ros_ws/src/crazyswarm/scripts/niceHover.py
+++ ros_ws/src/crazyswarm/scripts/niceHover.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
         pos = np.array(cf.initialPosition) + np.array([0, 0, Z])
         cf.goTo(pos, 0, 1.0)
 
-    print("press button to continue...")
-    swarm.input.waitUntilButtonPressed()
+    # print("press button to continue...")
+    # swarm.input.waitUntilButtonPressed()
 
+    print("Landing now!")
     allcfs.land(targetHeight=0.02, duration=1.0+Z)
     timeHelper.sleep(1.0+Z)
