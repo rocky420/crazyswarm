9cba5b5858a4bcf9f4f31499358f97cc2ea1ea0e
Enrica <enrica.s92@gmail.com>
2019-11-21 15:43:45 +0100

    Added files for fancy_traj and heart trajectory

--- ros_ws/src/crazyswarm/scripts/niceHover.py
+++ ros_ws/src/crazyswarm/scripts/niceHover.py
@@ -15,6 +15,12 @@ if __name__ == "__main__":
     for cf in allcfs.crazyflies:
         pos = np.array(cf.initialPosition) + np.array([0, 0, Z])
         cf.goTo(pos, 0, 1.0)
+    timeHelper.sleep(1.5+Z)
+
+    # for cf in allcfs.crazyflies:
+    #     pos = np.array(cf.initialPosition) + np.array([1.0, 1.0, Z])
+    #     cf.goTo(pos, 0, 3.0)
+    # timeHelper.sleep(1.5+Z)
 
     # print("press button to continue...")
     # swarm.input.waitUntilButtonPressed()
