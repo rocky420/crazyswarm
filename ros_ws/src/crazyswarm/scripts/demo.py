c1742bb28967a82206399309b855e47525512d4c
Enrica <enrica.s92@gmail.com>
2019-11-26 10:14:40 +0100

    Demo script works,crazyflies.yaml changed

--- ros_ws/src/crazyswarm/scripts/demo.py
+++ ros_ws/src/crazyswarm/scripts/demo.py
@@ -2,29 +2,41 @@
 
 import numpy as np
 from pycrazyswarm import *
+import pandas as pd
 
 Z = 1.0
-NB_AGENTS = 6;
+NB_AGENTS = 6
+
+
+def id_to_idx(id):
+    return id-1
+
+
+def idx_to_id(idx):
+    return idx+1
+
 
 if __name__ == "__main__":
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
 
+    ids = np.asarray(range(NB_AGENTS)) + 1
+    cfs = [allcfs.crazyfliesById[i] for i in ids]
+
     # Takeoff
     allcfs.takeoff(targetHeight=Z, duration=1.0+Z)
     timeHelper.sleep(1.5+Z)
 
-    # Create circle
-    ids = range(NB_AGENTS) -1;
-    cfs = [allcfs.crazyfliesById[i] for i in ids]
-    for cf in allcfs.crazyflies:
-        target_position = 0;
-        pos = target_position + np.array([0, 0, Z])
-        cf.goTo(pos, 0, 1.0)
+    # Get circle target points from csv
+    df = pd.read_csv("demo/target1.csv", skiprows=0)
+    target_positions = df.values[:, 1::]
 
-    # print("press button to continue...")
-    # swarm.input.waitUntilButtonPressed()
+    # Go to circle positions
+    for cf in allcfs.crazyflies:
+        target_position = target_positions[id_to_idx(cf.id)]
+        cf.goTo(target_position, 0, 2.0)
+    timeHelper.sleep(2.5)
 
     # Landing
     allcfs.land(targetHeight=0.02, duration=1.0+Z)
